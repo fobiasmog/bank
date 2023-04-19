ca80cd517a8c87efa30d90a98b6d44ef770d4fc0
fobiasmog <zbrejkin@yandex.ru>
2023-04-19 21:57:14 +0200

    models, migrations

---
+++ db/migrate/20230419194722_create_accounts.rb
@@ -0,0 +1,10 @@
+class CreateAccounts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :accounts do |t|
+      t.decimal :balance
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
